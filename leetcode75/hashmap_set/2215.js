6b24312f4dc6996f1275cd8da6190d42a306a94b
enzowritescode <1328683+enzowritescode@users.noreply.github.com>
2024-03-25 10:26:59 -0600

    Add initial 2215

---
+++ leetcode75/hashmap_set/2215.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+    
+};
+
+/*---------- TESTING AND DEBBUGING CODE BELOW ----------*/
+function test() {
+    let nums1 = [1,2,3]
+    let nums2 = [2,4,6];
+    console.log(findDifference(nums1, nums2));
+
+    nums1 = [1,2,3,3];
+    nums2 = [1,1,2,2];
+    console.log(findDifference(nums1, nums2));
+}
+
+test();
