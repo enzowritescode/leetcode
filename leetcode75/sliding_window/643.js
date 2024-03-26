937b0f0e74d8c10eb0c94f77a77c121afeea893d
enzowritescode <1328683+enzowritescode@users.noreply.github.com>
2024-03-25 21:48:05 -0600

    Adding initial file for 643

---
+++ leetcode75/sliding_window/643.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    
+};
+
+/*---------- TESTING AND DEBBUGING CODE BELOW ----------*/
+function test() {
+    let nums = [1,12,-5,-6,50,3]
+    let k = 4;
+    console.log(findMaxAverage(nums, k));
+
+    nums = [5]
+    k = 1;
+    console.log(findMaxAverage(nums, k));
+}
+
+test();
