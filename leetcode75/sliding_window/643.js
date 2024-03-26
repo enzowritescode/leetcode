39ae945ca15b3ed2606104216fd0a5600e52e392
enzowritescode <1328683+enzowritescode@users.noreply.github.com>
2024-03-25 22:15:27 -0600

    643. Maximum Average Subarray I

--- leetcode75/sliding_window/643.js
+++ leetcode75/sliding_window/643.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 var findMaxAverage = function(nums, k) {
-    
+	// calculate initial sum and average
+	let sum = 0;
+	for (let i = 0; i < k; i++) {
+		sum += nums[i];
+	}
+	let maxAvg = sum/k;
+
+	// sliding window
+	for (let i = 0; i < nums.length - k; i++) {
+		sum += nums[i+k] - nums[i];
+		let avg = sum/k;
+
+		if (avg > maxAvg) {
+			maxAvg = avg;
+		}
+	}
+
+	return maxAvg;
 };
 
 /*---------- TESTING AND DEBBUGING CODE BELOW ----------*/
@@ -15,6 +32,10 @@ function test() {
 
     nums = [5]
     k = 1;
+    console.log(findMaxAverage(nums, k));
+
+    nums = [4,0,4,3,3];
+    k = 5;
     console.log(findMaxAverage(nums, k));
 }
 
